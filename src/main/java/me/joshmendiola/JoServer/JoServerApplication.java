4fed44bff4c396c820b321e5f6775063c64c01ae
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-03-16 21:32:39 -0500

    first commit

---
+++ src/main/java/me/joshmendiola/JoServer/JoServerApplication.java
@@ -0,0 +1,13 @@
+package me.joshmendiola.JoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/joshmendiola/JoServer/JoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joshmendiola.JoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
