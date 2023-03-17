7bedea17682c5e34e23f31a1c6d29b52f0c6f0df
PhilosophyWithJosh <joshmendiola@icloud.com>
2023-03-16 21:37:46 -0500

    added blog model

---
+++ src/main/java/me/joshmendiola/JoServer/model/Blog.java
@@ -0,0 +1,59 @@
+package me.joshmendiola.JoServer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "Blogs")
+public class Blog
+{
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Getter
+    @Setter
+    private UUID id;
+
+    @Getter
+    @Setter
+    private String author;
+
+    @Getter
+    @Setter
+    private Date date;
+
+    @Getter
+    @Setter
+    private String text;
+
+    @Getter
+    @Setter
+    private String title;
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Blog blog = (Blog) o;
+        return id != null && Objects.equals(id, blog.id);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return getClass().hashCode();
+    }
+}
